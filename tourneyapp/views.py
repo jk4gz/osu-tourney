5b7ef6109940d2f068e0c44d60e8432815802f23
jk4gz <josephkim32698@gmail.com>
2022-02-27 00:35:44 -0500

    playing with templates to start with forms

--- tourneyapp/views.py
+++ tourneyapp/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+
+from .models import Mapdata
 
 def index(request):
-    return HttpResponse("Sup world!")
+    map_list = Mapdata.objects.all()
+    template = loader.get_template('tourneyapp/index.html')
+    context = {
+        'map_list': map_list,
+    }
+    return render(request, 'tourneyapp/index.html', context)
