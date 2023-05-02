f4ae8710835fae57e9ebce9269ded2cb7e095bac
jk4gz <josephkim32698@gmail.com>
2023-05-02 17:22:07 -0400

    Small refactoring

--- tourneyapp/views.py
+++ tourneyapp/views.py
@@ -22,7 +22,7 @@ def get_token():
 
     return response.json().get('access_token')
 
-def getMapInfo(data):
+def get_map_info(data):
     token = get_token()
 
     # beatmap = 'https://osu.ppy.sh/beatmapsets/352570#osu/786018'
@@ -53,6 +53,12 @@ def getMapInfo(data):
     #print(beatmapset_full['beatmapset']['covers']['card'])
     return(beatmapset_data)
 
+def convert_to_hr(beatmap_data):
+    return None
+
+def convert_to_dt(beatmap_data):
+    return None
+
 def index(request):
     template = loader.get_template('tourneyapp/index.html')
     # if this is a POST request we need to process the form data
@@ -76,6 +82,10 @@ def index(request):
         dt2_response = ""
         dt3_response = ""
         dt4_response = ""
+        # FM
+        fm1_response = ""
+        fm2_response = ""
+        fm3_response = ""
         # TB
         tb_response = ""
 
@@ -115,108 +125,113 @@ def index(request):
         try:
             # NM
             if nm1_link != "":
-                nm1_response = getMapInfo(nm1_link)
+                nm1_response = get_map_info(nm1_link)
             if nm2_link != "":
-                nm2_response = getMapInfo(nm2_link)
+                nm2_response = get_map_info(nm2_link)
             if nm3_link != "":
-                nm3_response = getMapInfo(nm3_link)
+                nm3_response = get_map_info(nm3_link)
             if nm4_link != "":
-                nm4_response = getMapInfo(nm4_link)
+                nm4_response = get_map_info(nm4_link)
             if nm5_link != "":
-                nm5_response = getMapInfo(nm5_link)
+                nm5_response = get_map_info(nm5_link)
             if nm6_link != "":
-                nm6_response = getMapInfo(nm6_link)
+                nm6_response = get_map_info(nm6_link)
             # HD
             if hd1_link != "":
-                hd1_response = getMapInfo(hd1_link)
+                hd1_response = get_map_info(hd1_link)
             if hd2_link != "":
-                hd2_response = getMapInfo(hd2_link)
+                hd2_response = get_map_info(hd2_link)
             if hd3_link != "":
-                hd3_response = getMapInfo(hd3_link)
+                hd3_response = get_map_info(hd3_link)
             # HR
             if hr1_link != "":
-                hr1_response = getMapInfo(hr1_link)
+                hr1_response = get_map_info(hr1_link)
             if hr2_link != "":
-                hr2_response = getMapInfo(hr2_link)
+                hr2_response = get_map_info(hr2_link)
             if hr3_link != "":
-                hr3_response = getMapInfo(hr3_link)
+                hr3_response = get_map_info(hr3_link)
             # DT
             if dt1_link != "":
-                dt1_response = getMapInfo(dt1_link)
+                dt1_response = get_map_info(dt1_link)
             if dt2_link != "":
-                dt2_response = getMapInfo(dt2_link)
+                dt2_response = get_map_info(dt2_link)
             if dt3_link != "":
-                dt3_response = getMapInfo(dt3_link)
+                dt3_response = get_map_info(dt3_link)
             if dt4_link != "":
-                dt4_response = getMapInfo(dt4_link)
+                dt4_response = get_map_info(dt4_link)
             # FM
             if fm1_link != "":
-                fm1_response = getMapInfo(fm1_link)
+                fm1_response = get_map_info(fm1_link)
             if fm2_link != "":
-                fm2_response = getMapInfo(fm2_link)
+                fm2_response = get_map_info(fm2_link)
             if fm3_link != "":
-                fm3_response = getMapInfo(fm3_link)
+                fm3_response = get_map_info(fm3_link)
             # TB
             if tb_link != "":
-                tb_response = getMapInfo(tb_link)
+                tb_response = get_map_info(tb_link)
+            
+            context = {
+                # NM
+                'nm1': nm1_response,
+                'nm2': nm2_response,
+                'nm3': nm3_response,
+                'nm4': nm4_response,
+                'nm5': nm5_response,
+                'nm6': nm6_response,
+                # HD
+                'hd1': hd1_response,
+                'hd2': hd2_response,
+                'hd3': hd3_response,
+                # HR
+                'hr1': hr1_response,
+                'hr2': hr2_response,
+                'hr3': hr3_response,
+                # DT
+                'dt1': dt1_response,
+                'dt2': dt2_response,
+                'dt3': dt3_response,
+                'dt4': dt4_response,
+                # FM
+                'fm1': fm1_response,
+                'fm2': fm2_response,
+                'fm3': fm3_response,
+                # TB
+                'tb': tb_response,
+                'form': mapform,
+            }
         except Exception as e:
             print("whoopsie")
+        return render(request, 'tourneyapp/test.html')
     # if a GET (or any other method) we'll create a blank form
     else:
         mapform = MapDataForm()
-        # NM
-        nm1_response = ""
-        nm2_response = ""
-        nm3_response = ""
-        nm4_response = ""
-        nm5_response = ""
-        nm6_response = ""
-        # HD
-        hd1_response = ""
-        hd2_response = ""
-        hd3_response = ""
-        # HR
-        hr1_response = ""
-        hr2_response = ""
-        hr3_response = ""
-        # DT
-        dt1_response = ""
-        dt2_response = ""
-        dt3_response = ""
-        dt4_response = ""
-        # FM
-        fm1_response = ""
-        fm2_response = ""
-        fm3_response = ""
-        # TB
-        tb_response = ""
-    context = {
-        # NM
-        'nm1': nm1_response,
-        'nm2': nm2_response,
-        'nm3': nm3_response,
-        'nm4': nm4_response,
-        'nm5': nm5_response,
-        'nm6': nm6_response,
-        # HD
-        'hd1': hd1_response,
-        'hd2': hd2_response,
-        'hd3': hd3_response,
-        # HR
-        'hr1': hr1_response,
-        'hr2': hr2_response,
-        'hr3': hr3_response,
-        # DT
-        'dt1': dt1_response,
-        'dt2': dt2_response,
-        'dt3': dt3_response,
-        'dt4': dt4_response,
-        # FM
-        'fm1': fm1_response,
-        'fm2': fm2_response,
-        'fm3': fm3_response,
-        # TB
-        'tb': tb_response,
-        'form': mapform,
-    }
-    return render(request, 'tourneyapp/index.html', context)
+        context = {
+            # NM
+            'nm1': "",
+            'nm2': "",
+            'nm3': "",
+            'nm4': "",
+            'nm5': "",
+            'nm6': "",
+            # HD
+            'hd1': "",
+            'hd2': "",
+            'hd3': "",
+            # HR
+            'hr1': "",
+            'hr2': "",
+            'hr3': "",
+            # DT
+            'dt1': "",
+            'dt2': "",
+            'dt3': "",
+            'dt4': "",
+            # FM
+            'fm1': "",
+            'fm2': "",
+            'fm3': "",
+            # TB
+            'tb': "",
+            'form': mapform,
+        }
+        return render(request, 'tourneyapp/index.html', context)
